add33afdcfc8acba1ce240d7182a44812d06fb62
Widia Permata Sari <widiapsr1504@gmail.com>
2016-03-29 10:36:42 +0700

    Update Ketupat.java

--- Ketupat.java
+++ Ketupat.java
@@ -22,27 +22,27 @@ public class Ketupat {
     int n = 12/2;
     for(int i=1;i<=n;i++){
     for(int k=n;k>i;k--){
-    System.out.print(" ");
+    System.out.print("0");
     }
     for(int j=1;j<=l;j++){
     System.out.print("*");
     }
     l+=2;
 
-    System.out.println("");
+    System.out.println("0");
     }
     l -= 4;
 
     for(int i=(n-1);i>=1;i--){
     for(int k=i;k<=(n-1);k++){
-    System.out.print(" ");
+    System.out.print("0");
     }
     for(int j=l;j>=1;j--){
     System.out.print("*");
     }
     l-=2;
 
-    System.out.println("");
+    System.out.println("0");
         }
 
 }
